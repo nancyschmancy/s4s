00768db1cf8f1ec33c34a8a65cee6e27e5587d0f
Nancy Reyes <nreyes.sf@gmail.com>
2019-09-11 21:05:15 -0700

    Add login and signup feature

--- qa/views.py
+++ qa/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 
 def home(request):
     return render(request, 'qa/index.html')
+
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
